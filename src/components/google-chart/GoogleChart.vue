f0cd7a2da97d254cae5b0134b63e6dc628c7e0cf
Sam <uizyk@gmail.com>
2023-02-17 11:49:56 +0900

    make updateChartData() more concise

--- src/components/google-chart/GoogleChart.vue
+++ src/components/google-chart/GoogleChart.vue
@@ -1,17 +1,20 @@
 <template>
     <div id="container">
-        <GChart :type="type" :data="data" :options="options" />
+        <GChart :type="type" :data="picked" :options="options" />
         <div>
-            <input type="checkbox" checked v-model="one" @change="updateChartData"/>
-            <input type="checkbox" v-model="two" @change="updateChartData"/>
-            <input type="checkbox" v-model="three" @change="updateChartData"/>
+          <label>BTC</label>
+            <input type="checkbox" ref="BTC" @change="updateChartData"/>
+          <label>ETH</label>
+            <input type="checkbox" ref="ETH" @change="updateChartData"/>
+          <label>BCH</label>
+            <input type="checkbox" ref="BCH" @change="updateChartData"/>
         </div>
     </div>
   </template>
   
   <script>
   import { GChart } from 'vue-google-charts';
-  
+  import cryptoData from '../../assets/crypto-data';
   export default {
     name: 'GoogleChart',
     components: {
@@ -20,60 +23,40 @@
     data() {
       return {
         type: "LineChart",
-        data: [],
+        picked: [],
         options: 
         {  
-            title: 'Crypto Prices',
-            curveType: 'function',
-            legend: { position: 'bottom' },
-            width: 1100,
-            height: 700,
+          title: 'Crypto chart',
+          curveType: 'function',
+          legend: { position: 'bottom' },
+          width: 1100,
+          height: 700,
+          colors: ['gold','purple','green'],
         },
-        one: true,
-        two: false,
-        three: false
+        coins: [
+          { ref: 'BTC', name: 'BTC' },
+          { ref: 'ETH', name: 'ETH' },
+          { ref: 'BCH', name: 'BCH' }
+        ]
       };
     },
-    computed: {
-
-    },
     methods: {
-    updateChartData() {
-        this.data = [['Year']];
-            const years = ['2018', '2019', '2020', '2021'];
-
-            if (this.one) {
-                this.data[0].push('one')
-                this.data.push([years[0], 500]); // example one data for year 2018
-                this.data.push([years[1], 800]); // example one data for year 2019
-                this.data.push([years[2], 1000]); // example one data for year 2020
-                this.data.push([years[3], 3500]); // example one data for year 2021
-            }
-
-            if (this.two) {
-                this.data[0].push('two')
-                this.data[1].push(991); // example two data for year 2018
-                this.data[2].push(550); // example two data for year 2019
-                this.data[3].push(993); // example two data for year 2020
-                this.data[4].push(555); // example two data for year 2021
-            }
-
-            if (this.three) {
-                this.data[0].push('three')
-                this.data[1].push(1003); // example three data for year 2018
-                this.data[2].push(730); // example three data for year 2019
-                this.data[3].push(500); // example three data for year 2020
-                this.data[4].push(1980); // example three data for year 2021
-            }
-            if(this.data === ['year']){
-                this.data = []
-            }
-
+      updateChartData() {
+        this.picked = [['Year']];
+        for (let i = 0; i < cryptoData.length; i++){
+          this.picked.push([cryptoData[i].year]);
         }
-      // trigger chartData computed property update
+        this.coins.forEach((coin) => {
+          if (this.$refs[coin.ref].checked) {
+            this.picked[0].push(coin.name);
+            for (let i = 0; i < cryptoData.length; i++){
+              this.picked[i + 1].push(cryptoData[i][coin.name]);
+            }
+          }
+        });
+      },
     },
-    
-  };
+  }
   </script>
   
   <style scoped>
